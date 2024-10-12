ebfc4a700c128d8879832d82e1dd54a82ca6406b
Danial <danialbarbodjalali@gmail.com>
2024-10-12 22:55:59 +0330

    Add
    Selecting using querySelector

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,3 +1,8 @@
+const titleInput = document.querySelector("#title");
+const descriptionInput = document.querySelector("#description");
+const dateInput = document.querySelector("#data");
+const addNoteButton = document.querySelector("button");
+const noteList = document.querySelector(".p-1.mt-5"); 
 //id genetrator
 export function createID() {
     let dateNow = Date.now(); 
